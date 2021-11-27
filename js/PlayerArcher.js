5e98735bfbbb9b34e67accf6a2506cb06afd0ee4
vtb7 <88791316+vtb7@users.noreply.github.com>
2021-11-27 12:05:26 -0500

    Update PlayerArcher.js

--- js/PlayerArcher.js
+++ js/PlayerArcher.js
@@ -19,7 +19,14 @@ class PlayerArcher {
     var angle = this.body.angle;
     
     //add the code to move arrow up and down
-
+    if(keyIsDown(DOWN_ARROW) && angle < -73) {
+      angle += 1
+      Matter.Body.setAngle(this.body,angle)
+    }
+    if(keyIsDown(UP_ARROW) && angle > -103) {
+      angle -=1
+      Matter.Body.setAngle(this.body,angle)
+    }
     push();
     translate(pos.x, pos.y);
     rotate(angle);
